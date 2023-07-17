0793340be190328c116c7b27f482c40d3b1ae57b
lucas viegas <90990096+lucasviegas1017@users.noreply.github.com>
2023-07-17 12:00:48 -0300

    Create void_troca.cpp

---
+++ trabalho_1/void_troca.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+int x =0;
+int y=0;
+	void *pont;
+cout<<"os valores das variaveis do tipo int e float, respectivamente, sao: "<<x<<" e "<<y<<endl<<"agora, utilizando um ponteiro para mudar o valor das variaveis, \ninsira o valor da variavel int: ";
+	pont=&x;
+	cin>>*(int*)pont; 
+cout<<"agora insira o valor da variavel float: ";
+	pont=&y;
+	cin>>*(int*)pont;
+cout<<"os valores mudados das variaveis do tipo int e float, respectivamente, sao "<<x<<" e "<<y<<endl;
+ return 0;	
+ }
+
+
