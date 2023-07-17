4fbc88451e4d0d623ae4001bcb014c70fd036697
lucas viegas <90990096+lucasviegas1017@users.noreply.github.com>
2023-07-17 12:05:49 -0300

    Add files via upload

---
+++ ptova/pilhaestaticaprova.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include<cstdlib>
+#include<cstddef>
+using namespace std;
+#define size 40
+typedef int tipochave;
+struct elemento 
+{
+    tipochave chave;
+    float preco;
+};
+struct No 
+{           
+    elemento valor;
+    No *anterior; 
+}; 
+
+class pilhaED
+{
+    private:
+        elemento *dado;
+        int tamanho;
+        No *NoTopo;
+    
+    public:
+        pilhaED(); // função criadora
+        bool cheia (); //1 cheia 0 não cheia
+        bool vazia (); // 1 vazia 0 não vazia
+        ~pilhaED(); // fun��o destrutora
+        void inserir (elemento e); // push
+        tipochave remover(); //pop
+        void mostrar ();
+        int gettamanho (); 
+        bool busca (tipochave ch);
+};
+
+int main()
+{
+    pilhaED p1,p2;
+    elemento e1;
+    tipochave ch1;
+    int menu,menu1;
+   while(true){
+    cout<<"faça uma escolha \n";
+    cout<<"digite 0 para sair do programa\ndigite 1 para usar a pilha 1\ndigite 2 para usar a pilha 2\n";
+    cin>>menu1;
+    switch(menu1)
+    {
+    case 0:
+    return 0;
+    break;
+    case 1:
+    do{
+       cout<<"digite: \n";
+        cout<<" 0 para voltar para o menu anterior \n";
+        cout<<" 1 para inserir um elemento \n"; 
+        cout<<" 2 para remover um elemento \n"; 
+        cout<<" 3 para ver todos os elementos da pilha\n"; 
+        cout<<" 4 para ver o tamanho da pilha \n"; 
+        cout<<" 5 para buscar por um elemento \n";
+        cout<<" 6 para destruir a pilha \n";
+        cin>>menu;
+        switch (menu)
+        {
+            case 0:
+            break;
+            case 1: cout<<"digite a chave do elemento a ser inserido";
+                     cin>>e1.chave;
+                     cout<<"digite o preco do elemento\n";
+                     cin>>e1.preco;
+                     p1.inserir (e1);  break;
+            case 2: cout<<p1.remover() <<"\n";  break;
+            case 3:  p1.mostrar();  break;
+            case 4:  cout<< "o tamanho da pilha é: " <<p1.gettamanho() <<"\n";
+                     break;
+            case 5: cout<<"digite a chave do elemento a ser procurado\n";
+                cin>>ch1;
+                if (p1.busca(ch1)==1){  cout<<"o elemnto já está na pilha\n"; }
+                else { cout<<"o elemnto não está na pilha\n";} 
+				break;
+				case 6: p1.~pilhaED(); 
+				break;
+            default: cout<<"opção invalida \n"; break;
+        }
+    } while(menu!=0);
+    break;
+    case 2:
+    do{
+       cout<<"digite: \n";
+        cout<<" 0 para terminar \n";
+        cout<<" 1 para inserir um elemento \n"; 
+        cout<<" 2 para remover um elemento \n"; 
+        cout<<" 3 para ver todos os elementos da pilha\n"; 
+        cout<<" 4 para ver o tamanho da pilha \n"; 
+        cout<<" 5 para buscar por um elemento \n";
+        cout<<" 6 para destruir a pilha \n";
+        cin>>menu;
+        switch (menu)
+        {
+            case 0: cout<<"The End\n"; break;
+            case 1: cout<<"digite a chave do elemento a ser inserido";
+                     cin>>e1.chave;
+                     cout<<"digite o preco do elemento\n";
+                     cin>>e1.preco;
+                     p1.inserir (e1);  break;
+            case 2: cout<<p1.remover() <<"\n";  break;
+            case 3:  p1.mostrar();  break;
+            case 4:  cout<< "o tamanho da pilha é: " <<p1.gettamanho() <<"\n";
+                     break;
+            case 5: cout<<"digite a chave do elemento a ser procurado\n";
+                cin>>ch1;
+                if (p1.busca(ch1)==1){  cout<<"o elemnto já está na pilha\n"; }
+                else { cout<<"o elemnto não está na pilha\n";} 
+				break;
+				case 6: p1.~pilhaED(); 
+				break;
+            default: cout<<"op��o invalida \n"; 
+			break;
+        }
+        
+    } while(menu!=0);
+    break;
+    default:break;
+    }
+   }
+    return 0;
+}
+pilhaED::pilhaED ()
+{
+    tamanho=0; 
+    dado=new elemento [size];
+}
+
+bool pilhaED::cheia ()
+{
+	No *NoNovo;
+    try
+ {
+     NoNovo= new No; 
+     delete NoNovo; 
+     return false; 
+     
+ }
+ catch(bad_alloc exeption)
+ {
+     return true; 
+ }
+
+}
+bool pilhaED::vazia()
+{
+     if(NoTopo==NULL) {return true;}
+    else { return false;}
+}
+bool pilhaED::busca (tipochave ch)
+{
+     int i;
+    for(i=0; i<tamanho; i++)
+    {
+        if (dado[i].chave==ch) 
+        {
+            return true;
+        }
+    }
+     return false;
+}
+void pilhaED::inserir (elemento e)
+{
+    int a;
+    a=cheia(); // para inserir primeiro devemos checar se não está cheia
+    if(a==1) 
+    {
+        cout<<"a pilha já está cheia\n";
+     //se está cheia não podemos inserir  
+    }
+    else
+    {  /// se não está cheia, antes de inserir devemos procurar se o elemento 
+        // já está na pilha
+        if (busca (e.chave)==1) // se ele está, não podemos inserir
+        {
+            cout<<"o elemento com chave " <<e.chave << " já está na pilha\n";
+        }
+        else
+        {
+        dado [tamanho]=e; //inserimos o elemento no topo
+        tamanho=tamanho+1; // aumentamos a quantidade de elementos
+        }
+    }
+}
+tipochave pilhaED::remover()
+{
+    int a;
+    a=vazia(); //só poderemos remover se a pilha não estiver vazia
+    if(a==1) // se a função retornar 1, então a pilha está vazia
+    {
+        cout<<" Não foi possivel remover: a pilha já estã vazia\n";
+         return 0;
+    }
+    else //se o retorno não é 1, então podemos remover o elemento do topo
+    {
+        tamanho=tamanho-1;
+        cout<<"foi removido o elemento com chave: \n";
+        return dado[tamanho].chave; 
+        //retornamos a chave do elemento que foi removido
+    }
+}
+void pilhaED::mostrar ()
+{
+    cout<<"os elementos da pilha são:\n";
+    int i;
+    for(i=0; i<tamanho; i++)
+    {
+        cout<<dado[i].chave  <<" \t" <<dado[i].preco<<"\n";
+    }
+
+}
+ pilhaED::~pilhaED ()
+{
+     No *NoTemp;
+    while (NoTopo!=NULL)
+    {
+        NoTemp=NoTopo;
+        NoTopo=NoTopo->anterior;
+        delete NoTemp;
+    }
+}
+int pilhaED::gettamanho ()
+{
+    return tamanho;
+}
+
